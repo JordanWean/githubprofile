b62406b61185468601d16f6c79b6d85721279822
Jordan Wean <wean.jordan@gmail.com>
2025-02-25 14:32:38 -0500

    Quartz sync: Feb 25, 2025, 2:32 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      Seeley Lab: "https://medresearch.umich.edu/labs-departments/labs/seeley-lab",
     },
   }),
 }
